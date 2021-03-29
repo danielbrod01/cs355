9efd1469c972f40e0ee48cc9e548aaa6c376a125
Daniel Rodriguez <daniel.b.rodriguez@gmail.com>
2021-03-28 22:06:49 -0400

    updated display information to include feels like temperature

--- weather/weather.js
+++ weather/weather.js
@@ -18,6 +18,7 @@ $(document).ready(function () {
         var URL = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`;
 
         $.getJSON(URL, function(data) {
+            console.log(data);
             updateDOM(data);
         });
     }
@@ -25,12 +26,14 @@ $(document).ready(function () {
     // Update Dom
     function updateDOM(data) {
         var city = data.name;
-        var temp = Math.round(data.main.temp_max);
+        var temp = Math.round(data.main.temp);
+        var feelsLike = Math.round(data.main.feels_like);
         var desc = data.weather[0].description;
         var icon = data.weather[0].icon;
 
         $('#city').html(city);
         $('#temp').html(temp);
+        $('#feels-like-temp').html(feelsLike);
         $('#desc').html(desc);
         $('#icon').attr('src', icon);
     }
